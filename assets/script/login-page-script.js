1dd63cb750857dc78d8d424fa773ed099d844924
Abhinav Gupta <abhi.mechkiit@gmail.com>
2023-03-21 18:45:54 +0530

    draw.io

--- assets/script/login-page-script.js
+++ assets/script/login-page-script.js
@@ -1,7 +1,6 @@
 function checkUser(){
    var userEmailID= document.getElementById("email").value;
    var userPassword = document.getElementById("password").value;
-   console.log(userEmailID.length)
    if(userEmailID.length === 0 || userPassword.length === 0)
    {
     window.alert("Please enter mandatory fields");
@@ -13,8 +12,10 @@ function checkUser(){
         window.location.href="./user-landing-page.html";
     }  
     else{
-        document.getElementById("email-label").style.color="red";
-        document.getElementById("pwd-label").style.color="red";
+        document.getElementById("email-label").style.color="#BD6367";
+        document.getElementById("pwd-label").style.color="#BD6367";
+        document.getElementById("invalid-user-email").style.display = "inline-block";
+        document.getElementById("invalid-user-pwd").style.display = "inline-block";
         document.getElementById("email").value = "";
         document.getElementById("password").value = "";
     }  
